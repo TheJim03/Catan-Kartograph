6ec79a2aad7ad399ce0ff9087661f1a571d24650
jimishere <105871542+TheJim03@users.noreply.github.com>
2026-07-08 02:00:37 +0200

    add action

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig(({ mode }) => ({
+  base: './',
   plugins: [react(), ...(mode === 'single' ? [viteSingleFile()] : [])],
   build: mode === 'single' ? { outDir: 'dist-single' } : {},
 }));
